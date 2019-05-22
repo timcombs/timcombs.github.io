edbf2725e218aaae393fb461b43013f34dc9d03e
Tim Combs <tim00combs@gmail.com>
2019-05-21 17:19:37 -0700

    added font-awesome npm packages and added fonts to navbar buttons. styled navbar buttons to match app window

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
 import './NavBar.css';
+
+library.add(fab);
 
   function NavBar(props) {
     return (
       <nav className='navbar'>
-        <button className='navbutton' name='linkedin' onClick={props.onClick}></button>
-        <button className='navbutton' name='github' onClick={props.onClick}></button>
+        <button className='navbutton' name='linkedin' onClick={props.onClick}><FontAwesomeIcon icon={['fab', 'linkedin']} /></button>
+        <button className='navbutton' name='github' onClick={props.onClick}><FontAwesomeIcon icon={['fab', 'github']} /></button>
         <button className='navbutton' name='about' onClick={props.onClick}>About</button>
-        <button className='navbutton' name='blog' onClick={props.onClick}></button>
+        <button className='navbutton' name='blog' onClick={props.onClick}>Blog</button>
       </nav>
     );
 }
