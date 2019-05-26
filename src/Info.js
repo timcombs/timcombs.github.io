06ca38f5f5382c86276bd7b31a4df847f115cc82
Tim Combs <tim00combs@gmail.com>
2019-05-26 12:30:35 -0700

    added some fonts to the repo. used font on Info, Quotes and TeamProj components. moved all font importing to App.css

--- src/Info.js
+++ src/Info.js
@@ -8,8 +8,8 @@ function Info() {
     <div className='infocontainer'>
     <section className='info'>
       <div className='name'>Tim Combs</div>
-      <div className='interests'>Art/Code</div>
-      <div className='interests'>JS developer working on remote & in-person collaborative teams</div>
+      <div className='interests'>Art/Code/Collaborative Teams</div>
+      <div className='interests'>JS developer interested in remote & in-person opportunities</div>
       <div className='motto'>Making places better while stewarding technology toward social good</div>
     </section>
     <NavBar />
