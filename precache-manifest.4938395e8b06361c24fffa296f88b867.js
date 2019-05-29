8080ea6ff12f916e34a0b261234e30e89c61b1d5
Tim Combs <tim00combs@gmail.com>
2019-05-29 07:17:39 -0700

    Updates

---
+++ precache-manifest.4938395e8b06361c24fffa296f88b867.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "792dd0d3e7b3157a444b7b7f354f44fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c2c300721fe2de5fe98",
+    "url": "/static/css/main.fecacdd6.chunk.css"
+  },
+  {
+    "revision": "a27e7396240164d9737e",
+    "url": "/static/js/2.b3578382.chunk.js"
+  },
+  {
+    "revision": "7c2c300721fe2de5fe98",
+    "url": "/static/js/main.b345e5ed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eb2bec254fb2dcb802d38ced4d5d6318",
+    "url": "/static/media/Lekton-Italic.eb2bec25.ttf"
+  },
+  {
+    "revision": "fc321a448098279cffec062000d0cc9f",
+    "url": "/static/media/Rueck-Left.fc321a44.ttf"
+  },
+  {
+    "revision": "459495acf75283551b04ef2d842b9b1b",
+    "url": "/static/media/gittherepdx.459495ac.png"
+  },
+  {
+    "revision": "c7597c26be4a932c9e944889ae373c55",
+    "url": "/static/media/imbibe-or.c7597c26.png"
+  },
+  {
+    "revision": "6120be0c9c3dcff051b531e33ed3f449",
+    "url": "/static/media/neverfind.6120be0c.png"
+  },
+  {
+    "revision": "a2274a58142cbdf17a1f93833651d6ec",
+    "url": "/static/media/portamento.a2274a58.png"
+  },
+  {
+    "revision": "0399ab68d1cff36025b56efacadd8790",
+    "url": "/static/media/sansforgetica-reg.0399ab68.otf"
+  },
+  {
+    "revision": "3274ec9e7becbe290187c02dd1291eb9",
+    "url": "/static/media/thestream.3274ec9e.png"
+  }
+]);
